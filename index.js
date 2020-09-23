107e42aa0fd7d3f89a6fcf8bbcd69c81536ec72d
Eden Smith <edenwint@gmail.com>
2020-09-23 18:32:44 +0100

    second commit

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
 import {books} from "./books.js";
 
 
-// function display(books) {
-//     books.map((book)=> {
-//         let titleElement = document.createElement("h1");
-//         let titleText = document.createTextNode(book.name);
-//         titleElement.appendChild(titleText);
-//         document.querySelector("#display-books").appendChild(titleElement);
-
-
-//         let authorElement = document.createElement("h2");
-//         let authorText = document.createTextNode(item.author);
-//         authorElement.appendChild(authorText);
-//         document.querySelector("#display-books").appendChild(authorElement);
-
-
-//         let priceElement = document.createElement("span");
-//         let priceText = document.createTextNode(item.price);
-//         priceElement.appendChild(priceText);
-//         document.querySelector("#display-books").appendChild(descriptionElement);
-
-
-//         let descriptionElement = document.createElement("p");
-//         let descriptionText = document.createTextNode(item.description);
-//         descriptionElement.appendChild(descriptionText);
-//         document.querySelector("#display-books").appendChild(descriptionElement);
-
-
-//         let imageElement = document.createElement("img");
-//         imageElement.src = item.image;
-//         imageElement.height = 260;
-//         imageElement.width = 150;
-
-
-//         document.querySelector("#display-books").appendChild(imageElement);
-//     })
-// }
-
-// display(books);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function createListOfBooks(books) {
 
@@ -60,7 +8,6 @@ function createListOfBooks(books) {
     const title = document.createElement("h2");
     const titleText = document.createTextNode("Suggested Reading");
     const booksDiv = document.createElement("div");
-
 
     books.map((book) => {
         const bookDivItem = document.createElement("div");
@@ -109,5 +56,4 @@ function createListOfBooks(books) {
 
 }
 
-
-createListOfBooks()
+createListOfBooks(books)
